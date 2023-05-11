44ef9bea5ec9d0b730dbce70ce545033971e287f
Omar Bouattour (ext.) <Omar.Bouattour@partner.bmw.de>
2023-05-11 13:37:08 +0200

    readme file updated and AK_MCS working

--- AK_MCS_U.py
+++ AK_MCS_U.py
@@ -1,9 +1,9 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import ConstantKernel, RBF
+
+
 def performance_function(x1, x2, k):
-    
     term1 = 3 + 0.1 * (x1 - x2)**2 - (x1 + x2)/(np.sqrt(2))
     term2 = 3 + 0.1 * (x1 - x2)**2 + (x1 + x2)/(np.sqrt(2))
     term3 = (x1 - x2) + k / (2**0.5)
@@ -12,9 +12,10 @@ def performance_function(x1, x2, k):
     return min(term1, term2, term3, term4)
 # Stage 1: Generation of Monte Carlo population
 nMC = 1000000
-S = np.random.rand(nMC, 2)
-
-
+x1 = np.random.normal(0,1,size = nMC)
+x2 = np.random.normal(0,1,size = nMC )
+S = np.column_stack((x1, x2))
+function_calls = 0
 k = 7
 
 
@@ -22,53 +23,59 @@ k = 7
 
 # Stage 2: Definition of initial design of experiments (DoE)
 N1 = 12
-DoE = S[:N1]
+selected_indices = np.random.choice(len(S), N1, replace=False)
+
+DoE = S[selected_indices]
 Pf_values = np.zeros(N1)  # Array to store performance function evaluations
 for i in range(N1):
     Pf_values[i] = performance_function(DoE[i, 0], DoE[i, 1], k)  # Evaluate performance function
-
+    function_calls += 1
 
 
 
 # Stage 3: Computation of Kriging model
 scaler = StandardScaler()
 scaled_DoE = scaler.fit_transform(DoE)
-kernel = ConstantKernel(1.0) * RBF(1.0)
-kriging = GaussianProcessRegressor(kernel=kernel)
+#kernel = ConstantKernel(1.0) * RBF(1.0)
+kriging = GaussianProcessRegressor()
 kriging.fit(scaled_DoE, Pf_values)
 while True:
     # Stage 4: Prediction by Kriging and estimation of probability of failure
     nMC = len(S)
-    G_hat = kriging.predict(scaler.transform(S))
-    Pf_hat = np.sum(G_hat <= 0) / nMC
-
+    G_hat, kriging_std = kriging.predict(scaler.transform(S),return_std=True)
+    Pf_hat = np.sum(G_hat < 0) / nMC
+    
     # Stage 5: Identification of the best next point to evaluate
-    learning_values = np.abs(G_hat) / np.sqrt(kriging.kernel_.k1.get_params()['constant_value'])
+    learning_values = np.abs(G_hat) / kriging_std
     x_best_index = np.argmin(learning_values)
     x_best = S[x_best_index]
-
     # Stage 6: Stopping condition on learning
-    stopping_condition = min(learning_values) >= 2
+    stopping_condition = min(learning_values) >= 2   
 
     # Stage 7: Update of the previous design of experiments with the best point
     if stopping_condition:
         # Stopping condition met, learning is stopped
-        cov_pf = np.sqrt(1 - Pf_hat) / (np.sqrt(Pf_hat) * nMC)
+        cov_pf = np.sqrt(1 - Pf_hat) / (np.sqrt(Pf_hat* nMC) )
+        
         cov_threshold = 0.05
         if cov_pf < cov_threshold:
             # Coefficient of variation is acceptable, stop AK-MCS
             print("AK-MCS finished. Probability of failure: ", Pf_hat)
             print("Coefficient of variation: ", cov_pf)
+            print("Number of calls to the performance function", function_calls)
             break
             # Stage 10: End of AK-MCS
         else:
             # Coefficient of variation is too high, update population
-            new_points = np.random.rand(nMC, 2)
+            new_x = np.random.normal(0,1, nMC)
+            new_y = np.random.normal(0,1,nMC)
+            new_points = np.column_stack((new_x, new_y)) 
             S = np.vstack((S, new_points))
             # Go back to Stage 4
     else:
         # Stopping condition not met, update design of experiments
         x_best_performance = performance_function(x_best[0], x_best[1], k)
+        function_calls += 1
         Pf_values = np.concatenate((Pf_values, [x_best_performance]))
         DoE = np.vstack((DoE, x_best))
         scaled_DoE = scaler.transform(DoE)
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# SRA
+*Monte_Carlo_Simulation.py contains the naive implementation of SRA using monte carlo simulation.
+nMC=10**6 samples were randomly generated to simulate the parameters of the performance function
+The simulation estimates a probability of failure of  0.002222 after calling the performance function 10**6 times.
+*AK_MCS_U.py implements the first surrogate to the monte carlo simulation using active learning method of kriging. 
+This implementation goes through the 10 stages discussed in the paper of Echard and al.
+1/ The population S (set to 10**6) is generated following a normal distribution 
+2/ N1 samples are randomly chosen, N1 is set to 12, to create the Domaine of Experience DoE. The performance function is evaluated in N1 points.
+3/ The kriging model is fit to the N1 data points
+4/ The kriging model is used to predict G_hat for the whole population S, which are the predcitions of the performance function by the kriging model.
+5/ The active learning step consists of choosing the next best point to evaluate, with respect to the learning function U.
+6/ Stopping criterion: Stop when the best learning value reach a maximum value of 2, i.e. min(U)>=2.
+    7/else: evaluate the performance function on the next best point x* and add it to the DoE. go back to 3
+    8/compute the coefficient of variance cov_pf. If it reaches the mthreshhold of 0.05, the AK_MCS has then converged. Go to 10.    
+        9/ Add new data points to  the population using monte carlo samples, and go back to 4.
+10/ print the results and end the iteration.
