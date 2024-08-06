b21b6c6706be157847b653177f28d64a4d0f7c7f
Omar Bouattour <omar.bouattour@yahoo.fr>
2024-08-06 22:17:24 +0200

    ..

--- four_branches_example/25.py
+++ four_branches_example/25.py
@@ -46,7 +46,7 @@ def min_distances_from_doe_vectorized(S, D):
 
 all_values = []
 all_f = []
-for _ in range(1):
+for _ in range(25):
     alpha = 0.75
     nMC = 500000
     x1 = np.random.normal(0, 1, size=nMC)
